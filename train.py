29412da27e8ab1631df707e5bdd379c9bb6cdf07
Karolis Rutkauskas <karolisrutkauskas1@gmail.com>
2021-05-16 13:58:03 +0300

    undo to timm vit

--- train.py
+++ train.py
@@ -1,20 +1,21 @@
+import timm
+from timm.utils import CheckpointSaver
 import torch
+import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
 import torch.nn as nn
 import numpy as np
 
-from transformers import BartForConditionalGeneration as BCD, BartTokenizerFast as BTF, ViTModel, ViTFeatureExtractor
+from transformers import BartForConditionalGeneration as BCD, BartTokenizerFast as BTF
 
 import dataset
 
 import sys
 
 def run_train(batch_size, learn_rate, number_of_epochs, do_eval):
-    # vit = timm.create_model('vit_base_patch32_384', pretrained=True, num_classes=0)
-    feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-384')
-    vit = ViTModel.from_pretrained('google/vit-base-patch16-384')
-    # freeze_vit(vit)
+    vit = timm.create_model('vit_base_patch32_384', pretrained=True, num_classes=0)
+    freeze_vit(vit)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(device)
 
@@ -28,7 +29,7 @@ def run_train(batch_size, learn_rate, number_of_epochs, do_eval):
         transforms.Resize((384, 384)),
         transforms.ToTensor(),
         transforms.Lambda(dataset.make_img_rgb),
-        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)),
+        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
         ])
 
     data_set = dataset.NarrativesDataset(root='./data/images/', file='./data/dataset.jsonl', transform=transform)
@@ -52,6 +53,8 @@ def run_train(batch_size, learn_rate, number_of_epochs, do_eval):
     
     optimizer = optim.Adam(list(vit.parameters()) + list(bart.parameters()), lr=learn_rate)
 
+    cs = CheckpointSaver(vit, optimizer, checkpoint_dir='data/checkpoints')
+
     for epoch in range(number_of_epochs):
         running_loss = 0.0
         number_of_iterations = 0
@@ -59,15 +62,14 @@ def run_train(batch_size, learn_rate, number_of_epochs, do_eval):
             optimizer.zero_grad()
             images, narratives = data
             images = images.to(device)
-            images = to_list(images)
 
-            inputs = feature_extractor(images=images, return_tensors="pt")
-            image_features = vit(**inputs).last_hidden_state
+            print(images.shape)
 
+            image_features = vit.forward_features(images)
             tokenized_data = tokenizer.prepare_seq2seq_batch("", list(narratives), padding=True, truncation=True).data
             labels = torch.tensor(tokenized_data['labels']).to(device)
 
-            image_features = torch.unsqueeze(image_features, 0).to(device)
+            image_features = torch.unsqueeze(torch.unsqueeze(image_features, 1), 0).to(device)
 
             bart_outputs = bart(encoder_outputs=image_features, labels=labels)
             
@@ -82,15 +84,15 @@ def run_train(batch_size, learn_rate, number_of_epochs, do_eval):
 
         if do_eval:
             print('Running eval...')
-            run_eval(vit, feature_extractor, bart, valloader, device, tokenizer)
+            run_eval(vit, bart, valloader, device, tokenizer)
             
     print('Finished Training')
     print('Saving model...')
 
-    vit.save_pretrained('data/vit')
+    cs.save_checkpoint(1)
     bart.save_pretrained('data/bart')
 
-def run_eval(vit, feature_extractor, bart, eval_loader, device, tokenizer):
+def run_eval(vit, bart, eval_loader, device, tokenizer):
     vit.eval()
     bart.eval()
     
@@ -101,10 +103,8 @@ def run_eval(vit, feature_extractor, bart, eval_loader, device, tokenizer):
         for i, data in enumerate(eval_loader, 0):
             images, narratives = data
             images = images.to(device)
-            images = to_list(images)
 
-            inputs = feature_extractor(images=images, return_tensors="pt")
-            image_features = vit(**inputs).last_hidden_state
+            image_features = vit.forward_features(images)
             tokenized_data = tokenizer.prepare_seq2seq_batch("", list(narratives), padding=True, truncation=True).data
             labels = torch.tensor(tokenized_data['labels']).to(device)
 
@@ -139,13 +139,6 @@ def freeze_bart(bart):
             for i in range(number_of_blocks, len(child[1])):
                 for param in child[1][i].parameters():
                     param.requires_grad = False
-
-def to_list(array):
-    image_list = []
-    for i in range(array.shape[0]):
-        image_list.insert(i, array[i])
-
-    return image_list
 
 if __name__ == '__main__':
     batch_size = int(sys.argv[1])
